17ee9ba9abb9b4dc83c6acf2e6d60d5e1d38f097
jose montes de oca <75558439+alchemyyt@users.noreply.github.com>
2025-04-21 21:26:47 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="es">
+      <meta name="google-adsense-account" content="ca-pub-1680603531729114">
       <body className={`${inter.className} `}>
         <div className=''>
           <NavegationMobile/>
